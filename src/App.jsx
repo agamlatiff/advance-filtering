a1bfec23d1c3fd325b103c66f4ec5b83268a7824
agamlatiff <agam.latiff@gmail.com>
2025-07-28 22:09:46 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navigation from "./Navigation/Nav";
+import Products from "./Products/Products";
+import Recommended from "./Recommended/Recommended";
+
+const App = () => {
+  return (
+    <>
+      <Navigation />
+      <Products />
+      <Recommended/>
+    </>
+  );
+};
+
+export default App;
